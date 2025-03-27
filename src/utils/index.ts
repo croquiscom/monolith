871660144793e2e558588a051ca23d0659aed974
lake.zero <lake.zero@kakaostyle.com>
2025-03-27 15:56:58 +0900

    feat: compressImage util 추가

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,8 @@
 /**
  * @packageDocumentation Monolith에서 구성하는 공통 유틸 함수들이 구성되어있는 영역 입니다.
  */
-export { createArray } from './create-array/createArray';
-export { delay } from './delay/delay';
-export { isIos } from './is-ios/isIos';
-export { isAndroid } from './is-android/isAndroid';
+export { createArray } from "./create-array/createArray";
+export { delay } from "./delay/delay";
+export { isIos } from "./is-ios/isIos";
+export { isAndroid } from "./is-android/isAndroid";
+export { compressImage } from "./compress-image/compressImage";
