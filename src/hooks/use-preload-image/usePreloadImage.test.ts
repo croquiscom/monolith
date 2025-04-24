10e88ae2aae96ea2c22862728b6216b298d63d99
sunginHwang <gommpo111@gmail.com>
2025-04-24 10:53:04 +0900

    feat: 이미지 로드 완료 플래그 추가

--- src/hooks/use-preload-image/usePreloadImage.test.ts
+++ src/hooks/use-preload-image/usePreloadImage.test.ts
@@ -1,24 +1,74 @@
-import { renderHook } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
 import { usePreloadImage } from './usePreloadImage';
 
-describe('usePreloadAsset 훅', () => {
-  it('주어진 이미지 리스트 갯수만큼 이미지를 preload 합니다.', () => {
-    const image_src_list = ['https://placehold.co/600x400', 'https://placehold.co/300x100'];
+ describe('usePreloadAsset 훅', () => {
+  let original_image: typeof Image;
 
-    const image_mock = vi.spyOn(global, 'Image');
+  beforeEach(() => {
+    original_image = global.Image;
+  
+    global.Image = class {
+      _src = '';
+      onload: () => void = () => {};
+      onerror: () => void = () => {};
 
-    renderHook(() => usePreloadImage(image_src_list));
 
-    expect(image_mock).toHaveBeenCalledTimes(image_src_list.length);
-    expect(image_mock).toHaveBeenCalledWith();
+
+      set src(value: string) {
+        this._src = value;
+  
+        setTimeout(() => {
+          if (value.includes('fail')) {
+            this.onerror();
+          } else {
+            this.onload();
+          }
+        }, 10);
+      }
+  
+      get src() {
+        return this._src;
+      }
+    } as unknown as typeof Image;
+  });
+
+  afterEach(() => {
+    global.Image = original_image;
+  });
+
+  it('주어진 이미지 리스트 갯수만큼 이미지를 preload 합니다. 전부 load되기 기전에는 반환값은  fasle를 반환합니다. 전부 load 이후 true를 반환 합니다.', async () => {
+    const { result } = renderHook(() =>
+      usePreloadImage(['img1.jpg', 'img2.jpg', 'img3.jpg'])
+    );
+
+    expect(result.current).toBe(false);
+
+    await act(async () => {
+      await new Promise((res) => setTimeout(res, 30));
+    });
+
+    expect(result.current).toBe(true); 
   });
 
   it('이미지 리스트가 빈배열로 주어진 경우 어떠한preload도 발생하지 않습니다.', () => {
     const image_list: string[] = [];
-    const image_mock = vi.spyOn(global, 'Image');
 
-    renderHook(() => usePreloadImage(image_list));
+    const {result} =renderHook(() => usePreloadImage(image_list));
+    expect(result.current).toBe(true);
+  });
 
-    expect(image_mock).not.toHaveBeenCalled();
+
+  it('중간에 실패한 이미지가 있어도 실패 시그널이 나온다면 전부 로드 완료로 반환합니다', async () => {
+    const { result } = renderHook(() =>
+      usePreloadImage(['img1.jpg', 'fail.jpg', 'img2.jpg'])
+    );
+
+    expect(result.current).toBe(false);
+
+    await act(async () => {
+      await new Promise((res) => setTimeout(res, 30));
+    });
+
+    expect(result.current).toBe(true); 
   });
 });
--- src/hooks/use-preload-image/usePreloadImage.ts
+++ src/hooks/use-preload-image/usePreloadImage.ts
@@ -1,16 +1,45 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 /**
  * 성능상의 이슈로 preload 가 필요한 이미지들의 경우
  * 해당 hook을 사용하여 인자로 주어진 이미지 리스트를 preload 합니다.
  *
  * @param image_src_list - preload 할 이미지 링크리스트
+ * @return is_preload_completed - preload 완료 여부
+ * 
+ * @example 
+ *  const is_completed_load_image = usePreloadImage(['https://example.com/image1.jpg', 'https://example.com/image2.jpg']);
+ * is_completed_load_image - true(전부 이미지 로드가 완료된 경우 true 아니면 false)
  */
 export const usePreloadImage = (image_src_list: string[]) => {
+  const [is_preload_completed, setIsPreloadCompleted] = useState(false);
+
   useEffect(() => {
+    let loadedCount = 0;
+
+    if (image_src_list.length === 0) {
+      setIsPreloadCompleted(true);
+      return;
+    }
+
+    const completeLoadImage = () => {
+      loadedCount++;
+      if (loadedCount === image_src_list.length) {
+        setIsPreloadCompleted(true);
+      }
+    };
+
     [...image_src_list].forEach((image) => {
       const img = new Image();
+
+      img.onload = completeLoadImage;
+      img.onerror = completeLoadImage;
+
       img.src = image;
     });
   }, [image_src_list]);
+
+  return is_preload_completed;
 };
+
+
